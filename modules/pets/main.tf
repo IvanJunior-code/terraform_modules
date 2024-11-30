62e1318337c2867305da7cb48da84e643c9664d1
Ivan Junior <ivan_junior_we@hotmail.com>
2024-11-29 22:26:06 -0300

    Ajuste no nome dos arquivos

--- modules/pets/main.tf
+++ modules/pets/main.tf
@@ -1,11 +1,11 @@
 resource "local_file" "file01" {
   content  = random_pet.pet01.id
-  filename = "./output-files/${var.prefix_file}-pet01-${random_pet.pet01.id}"
+  filename = "./output-files/${var.prefix_file}-pet-${random_pet.pet01.id}"
 }
 
 resource "local_file" "file02" {
   content  = random_pet.pet02.id
-  filename = "./output-files/${var.prefix_file}-pet02-${random_pet.pet02.id}"
+  filename = "./output-files/${var.prefix_file}-pet-${random_pet.pet02.id}"
 }
 
 resource "random_pet" "pet01" {
