4b66efe9633c0d44172db87bb9aeb20ecbd92907
Ivan Junior <ivan_junior_we@hotmail.com>
2024-11-29 22:25:46 -0300

    Ajuste no output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "pet01_name" {
-  value = module.pets.output_pet01_name #refers to modules output
+  value = module.pets[*].output_pet01_name #refers to modules output
 }
 
 output "pet02_name" {
-  value = module.pets.output_pet02_name
+  value = module.pets[*].output_pet02_name
 }
